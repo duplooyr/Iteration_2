d07303553a05f665810ee95a2901b6d6fbcedf54
Ryan Duplooy <duplooyra@s.dcsdk12.org>
2017-11-28 08:25:14 -0700

    Completed function switch_letters

--- functions.py
+++ functions.py
@@ -10,4 +10,18 @@ def replace_letter(word,letter1,letter2):
             n += 1
     return new_word
 #////////////////////////////////////////////////////////////////////////////
-def switch_letters():
+def switch_letters(word,letter1,letter2):
+    n = 0
+    new_word = ""
+    for count in range(0,len(word)):
+        if word[n] == letter1:
+            new_word = new_word + letter2
+            n += 1
+        elif word[n] == letter2:
+            new_word = new_word + letter1
+            n += 1
+        else:
+            new_word = new_word + word[n]
+            n += 1
+    return new_word
+#////////////////////////////////////////////////////////////////////////////
--- test.py
+++ test.py
@@ -1,2 +1,3 @@
 from functions import *
 print replace_letter("Hello","l","n")
+print switch_letters("Textbook","o","e")
