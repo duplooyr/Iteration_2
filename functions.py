4a0779e7be5be8188f333f8b481f01acfcc25291
Ryan Duplooy <duplooyra@s.dcsdk12.org>
2017-11-29 09:26:55 -0700

    Switch_words function is near complete

--- functions.py
+++ functions.py
@@ -25,3 +25,17 @@ def switch_letters(word,letter1,letter2):
             n += 1
     return new_word
 #////////////////////////////////////////////////////////////////////////////
+def switch_words(sentence,word1,word2):
+    n = 0
+    new_sentence = ""
+    for count in range(0,len(sentence)):
+        if sentence[n:n+len(word1)] == word1:
+            new_sentence = new_sentence + word2
+            n +=1
+        elif sentence[n:n+len(word2)] == word2:
+            new_sentence = new_sentence + word1
+            n +=1
+        else:
+            new_sentence = new_sentence + sentence[n]
+            n +=1
+    return  new_sentence
