34b648dd82184f1db9c5b83b0c0000a677164287
Ryan Duplooy <duplooyra@s.dcsdk12.org>
2017-12-01 08:16:39 -0700

    Completed function switch_words

--- functions.py
+++ functions.py
@@ -2,40 +2,52 @@ def replace_letter(word,letter1,letter2):
     n = 0
     new_word = ""
     for count in range(0,len(word)):
+
         if word[n] == letter1:
             new_word = new_word + letter2
             n += 1
+
         else:
             new_word = new_word + word[n]
             n += 1
+            
     return new_word
 #////////////////////////////////////////////////////////////////////////////
 def switch_letters(word,letter1,letter2):
     n = 0
     new_word = ""
     for count in range(0,len(word)):
+
         if word[n] == letter1:
             new_word = new_word + letter2
             n += 1
+            
         elif word[n] == letter2:
             new_word = new_word + letter1
             n += 1
+
         else:
             new_word = new_word + word[n]
             n += 1
+
     return new_word
 #////////////////////////////////////////////////////////////////////////////
 def switch_words(sentence,word1,word2):
     n = 0
     new_sentence = ""
-    for count in range(0,len(sentence)):
+    for count in range(0,len(sentence)-len(word1 + word2)+2):
+
         if sentence[n:n+len(word1)] == word1:
             new_sentence = new_sentence + word2
-            n +=1
+            n += len(word1)
+
         elif sentence[n:n+len(word2)] == word2:
             new_sentence = new_sentence + word1
-            n +=1
+            n += len(word2)
+
         else:
             new_sentence = new_sentence + sentence[n]
             n +=1
+
     return  new_sentence
+#////////////////////////////////////////////////////////////////////////////
--- test.py
+++ test.py
@@ -1,4 +1,4 @@
 from functions import *
 print replace_letter("Hello","l","n")
 print switch_letters("Textbook","o","e")
-print switch_words("The quick brown fox jumped over the lazy dog","fox","dog")
+print switch_words("The quick brown fox jumped over the lazy dog","jumped","over")
